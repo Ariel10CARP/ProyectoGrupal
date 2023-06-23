b3f1adee2173c040f3b6989f597095ce936004be
Deiviidf <135382016+Deiviidf@users.noreply.github.com>
2023-06-22 23:52:51 -0300

    Update eje2.ts

--- ejer1/eje2.ts
+++ ejer1/eje2.ts
@@ -21,3 +21,27 @@ interface Tarea {
         tarea.completada = true;
       }
     }
+       listarTareas(): void {
+    for (const tarea of this.tareas) {
+      console.log(`ID: ${tarea.id}, Descripción: ${tarea.descripcion}, Completada: ${tarea.completada}`);
+    }
+  }
+
+  private buscarTareaPorId(id: number): Tarea | undefined {
+    return this.tareas.find(tarea => tarea.id === id);
+  }
+}
+
+// Uso de la clase ListaTareas
+
+const lista = new ListaTareas();
+
+lista.agregarTarea({ id: 1, descripcion: 'Hacer la compra', completada: false });
+lista.agregarTarea({ id: 2, descripcion: 'Hacer ejercicio', completada: false });
+lista.agregarTarea({ id: 3, descripcion: 'Estudiar para el examen', completada: false });
+
+lista.listarTareas();
+
+lista.marcarTareaCompleta(2);
+
+lista.listarTareas();
